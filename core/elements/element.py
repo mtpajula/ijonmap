4654022c7feecfac56fa9c038dadef140c72d106
Mikko Pajula <mtpajula@gmail.com>
2014-05-03 21:37:15 +0300

    features dict and dia

--- core/elements/element.py
+++ core/elements/element.py
@@ -6,30 +6,30 @@ class Element(object):
     def __init__(self):
         self.id = None
         self.type = None
-        self.code = None
         self.table_id = None
+        self.features = {}
         
     def get_str(self):
         s = self.type + " id: "
         s += str(self.id)
-        s += " code: "
-        s += str(self.code)
+        s += " features: "
+        s += str(self.features)
         return s
         
     def get_dictionary(self):
         d = {
                     'id' : self.id,
                     'type' : self.type,
-                    'code' : self.code,
-                    'table_id' : self.table_id
+                    'table_id' : self.table_id,
+                    'features' : self.features
                     }
         return d
         
     def set_dictionary(self, d):
         self.id = d['id']
         self.type = d['type']
-        self.code = d['code']
         self.table_id = d['table_id']
+        self.features = d['features']
         
         
 
